eda8cbabd7a48598dce327701377de64d5b5cbb9
youngjunkim <kyj0703@konkuk.ac.kr>
2023-05-16 06:41:11 +0900

    first commit

---
+++ sy.js
@@ -0,0 +1,34 @@
+/*
+a Pen by DIACO : twitter.com/Diaco_ml  ||  codepen.io/MAW
+powered by GSAP : https://www.greensock.com/
+*/
+var w = window.innerWidth;
+var h = window.innerHeight;
+window.onload = function(){
+	TweenLite.set("#container",{perspective:600})
+	TweenLite.set("img",{xPercent:"-50%",yPercent:"-50%"})
+
+	var total = 30;
+	var warp = document.getElementById("container");
+
+	 
+	for (i=0; i<total; i++){ 
+		var Div = document.createElement('div');
+	  	TweenLite.set(Div,{attr:{class:'dot'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+	 	console.log(Div);
+	  	warp.appendChild(Div);
+	  	animm(Div);
+	}
+}
+
+
+function animm(elm){   
+  	TweenMax.to(elm,R(6,15),{y:h+100,ease:Linear.easeNone,repeat:-1,delay:-15});
+  	TweenMax.to(elm,R(4,8),{x:'+=100',rotationZ:R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
+  	TweenMax.to(elm,R(2,8),{rotationX:R(0,360),rotationY:R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
+};
+
+function R(min,max) {return min+Math.random()*(max-min)};
+
+
+// a Pen by DIACO : twitter.com/Diaco_ml  ||  codepen.io/MAW
